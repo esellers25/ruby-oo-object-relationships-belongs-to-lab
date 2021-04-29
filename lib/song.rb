aa9873f16bd6c8c3dd4090c1772adccb8cd44682
esellers25 <erin.e.sellers@gmail.com>
2021-04-29 16:33:18 -0400

    commit tree 5fe9d23b7d8a2e40dc7624f6467a3b233f1be7a7

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+
+    # @all = []
+    attr_accessor :title, :artist
+
+    # def initialize(title, artist)
+    #     @title = title 
+    #     @artist = artist
+    #     @all << self 
+    # end 
+
+    # def self.all
+    #     @all
+    # end 
+end 
